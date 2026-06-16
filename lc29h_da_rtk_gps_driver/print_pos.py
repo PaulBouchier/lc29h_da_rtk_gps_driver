e980beb3f411e94e3afd537e9345c49d9910f3e6
Paul Bouchier <paul.bouchier@gmail.com>
2026-06-16 09:37:16 -0500

    print X/Y to cm precision

--- lc29h_da_rtk_gps_driver/print_pos.py
+++ lc29h_da_rtk_gps_driver/print_pos.py
@@ -99,9 +99,9 @@ class PrintPos(Node):
         self.last_timer_xy_time = self.last_xy_time
 
         # Format values to 3 decimal places if available, otherwise 'None'
-        x_str = f'{self.x:.3f}' if self.x is not None else 'None'
-        y_str = f'{self.y:.3f}' if self.y is not None else 'None'
-        z_str = f'{self.z:.3f}' if self.z is not None else 'None'
+        x_str = f'{self.x:.2f}' if self.x is not None else 'None'
+        y_str = f'{self.y:.2f}' if self.y is not None else 'None'
+        z_str = f'{self.z:.2f}' if self.z is not None else 'None'
 
         # Print to stdout/logger
         output_str = (
